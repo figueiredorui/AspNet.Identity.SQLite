341f2395cb69a98588e7d2c0a525cfd6e8a3f0ca
Rui Figueiredo <figueiredo.rui@gmail.com>
2014-09-04 10:43:05 +0100

    Initial Commit

---
+++ src/AspNet.Identity.SQLite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNet.Identity.SQLite.Startup))]
+namespace AspNet.Identity.SQLite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
